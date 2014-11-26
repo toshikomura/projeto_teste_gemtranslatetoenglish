552db3e698e93972b0e6964471101746db5fd853
Gustavo Toshi Komura <gustavo.toshi.k@gmail.com>
2014-11-26 12:54:42 -0800

    Execute rails new projeto_teste_gemtranslatetoenglish
    Signe-off-by: Gustavo Toshi Komura <gustavo.toshi.k@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetoTesteGemtranslatetoenglish::Application.config.secret_token = 'e93c4237c7ce5b80d47947a8afdc817d26bd71f5c577a87bd95b80b73a7b21a2c9b441fd7492e4ebcb40d094395bb8d9d1e5824dcfa9cb89c6d251ef2d0af7c6'
